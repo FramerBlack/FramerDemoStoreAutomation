5b9e6479f43464d840ce9c5b98c895626122806f
Lezz <54168079+TheFireBrand@users.noreply.github.com>
2024-09-17 14:59:53 +0200

    Vite updated

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,23 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  root: '.', // Set the root to the current directory
   server: {
     host: '0.0.0.0',
-  }
-})
+  },
+  build: {
+    outDir: 'dist', // Output directory for the build
+  },
+  publicDir: 'public', // Directory to serve as static files
+});
+
+// import { defineConfig } from 'vite';
+// import react from '@vitejs/plugin-react';
+
+// // https://vitejs.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+//   server: {
+//     host: '0.0.0.0',
+//   }
+// })
